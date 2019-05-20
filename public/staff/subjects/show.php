8fcb06d9f9eafb53f9d47669e1e87a311467e071
Amulya Kumar Shahi <amulyaa_24@yahoo.in>
2019-05-20 20:53:41 +0530

    showed with single page id

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,18 +1,19 @@
+<?php require_once('../../../private/initialize.php'); ?>
 
 
-
-
-
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <main>
-<div class="go_back">
-	<a href="<?php echo url_for('/staff/subjects/index.php'); ?>">...Back</a>
-</div>
-
-
 	
+	<div class="go_back">
+		<a href="<?php echo url_for('/staff/subjects/index.php'); ?>">...Back</a>
+	</div>
 
-
-
+	<h2><?php $id = $_GET['id'] ?? '1'; ?>
+		<?php echo $id ?>
+	</h2>
 
 </main>
+
+
+<?php include(SHARED_PATH . "/staff_footer.php"); 
